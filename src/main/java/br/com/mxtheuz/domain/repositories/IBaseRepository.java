dd8651ce0431d87bf7a43ad9874dc3eeb765f1dc
Matheus Piccoli <90228238+ofmxtheuuz@users.noreply.github.com>
2023-09-24 16:22:48 -0300

    refact: remove spaces

--- src/main/java/br/com/mxtheuz/domain/repositories/IBaseRepository.java
+++ src/main/java/br/com/mxtheuz/domain/repositories/IBaseRepository.java
@@ -3,9 +3,7 @@ package br.com.mxtheuz.domain.repositories;
 import java.util.List;
 
 public interface IBaseRepository<T, L> {
-
     List<T> GetAll();
     T GetById(L id);
     T Save(T entity);
-
 }
